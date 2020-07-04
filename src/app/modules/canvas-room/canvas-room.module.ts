4af3a819ed98b87815af54925e30221169b8d32d
ehliesho <ehliesho@avans.nl>
2020-07-04 06:00:46 +0200

    changed component structure and added a minimap

--- src/app/modules/canvas-room/canvas-room.module.ts
+++ src/app/modules/canvas-room/canvas-room.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CanvasRoomPageComponent } from './pages/canvas-room-page/canvas-room-page.component';
 import { CanvasRoomRoutingModule } from './canvas-room-routing.module';
+import { RoomViewComponent } from './pages/canvas-room-page/components/room-view/room-view.component';
+import { MapViewComponent } from './pages/canvas-room-page/components/map-view/map-view.component';
 
 
 
 @NgModule({
-  declarations: [CanvasRoomPageComponent],
+  declarations: [CanvasRoomPageComponent, RoomViewComponent, MapViewComponent],
   imports: [
     CommonModule, CanvasRoomRoutingModule
   ]

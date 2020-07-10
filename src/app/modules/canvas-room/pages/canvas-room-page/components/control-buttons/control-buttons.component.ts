3cc25d28e8dc853e34540c62efa0bb2d0d7f222d
ehliesho <ehliesho@avans.nl>
2020-07-11 00:18:08 +0200

    touchmove and touchcancel

--- src/app/modules/canvas-room/pages/canvas-room-page/components/control-buttons/control-buttons.component.ts
+++ src/app/modules/canvas-room/pages/canvas-room-page/components/control-buttons/control-buttons.component.ts
@@ -24,11 +24,14 @@ export class ControlButtonsComponent {
   @Output()
   public stopMoveRight: EventEmitter<void> = new EventEmitter<void>();
 
+  public moving = false;
+
   public onStartMovingUp = (event?: Event): void => {
     if (event) {
       event.preventDefault();
     }
     this.startMoveUp.emit();
+    this.moving = true;
   };
 
   public onStartMovingDown = (event?: Event): void => {
@@ -36,6 +39,7 @@ export class ControlButtonsComponent {
       event.preventDefault();
     }
     this.startMoveDown.emit();
+    this.moving = true;
   };
 
   public onStartMovingLeft = (event?: Event): void => {
@@ -43,6 +47,7 @@ export class ControlButtonsComponent {
       event.preventDefault();
     }
     this.startMoveLeft.emit();
+    this.moving = true;
   };
 
   public onStartMovingRight = (event?: Event): void => {
@@ -50,21 +55,86 @@ export class ControlButtonsComponent {
       event.preventDefault();
     }
     this.startMoveRight.emit();
+    this.moving = true;
   };
 
   public onStopMovingUp = (): void => {
     this.stopMoveUp.emit();
+    this.moving = false;
   };
 
   public onStopMovingDown = (): void => {
     this.stopMoveDown.emit();
+    this.moving = false;
   };
 
   public onStopMovingLeft = (): void => {
     this.stopMoveLeft.emit();
+    this.moving = false;
   };
 
   public onStopMovingRight = (): void => {
     this.stopMoveRight.emit();
+    this.moving = false;
+  };
+
+  public onChangeMovingUp = (event?: Event): void => {
+    if (this.moving) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.startMoveUp.emit();
+      this.stopMoveLeft.emit();
+      this.stopMoveRight.emit();
+      this.stopMoveDown.emit();
+    }
+  };
+
+  public onChangeMovingDown = (event?: Event): void => {
+    if (this.moving) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.startMoveDown.emit();
+      this.stopMoveLeft.emit();
+      this.stopMoveRight.emit();
+      this.stopMoveUp.emit();
+    }
+  };
+
+  public onChangeMovingRight = (event?: Event): void => {
+    if (this.moving) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.startMoveRight.emit();
+      this.stopMoveLeft.emit();
+      this.stopMoveUp.emit();
+      this.stopMoveDown.emit();
+    }
+  };
+
+  public onChangeMovingLeft = (event?: Event): void => {
+    if (this.moving) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.startMoveLeft.emit();
+      this.stopMoveUp.emit();
+      this.stopMoveRight.emit();
+      this.stopMoveDown.emit();
+    }
+  };
+
+  public onChangeCancel = (event?: Event): void => {
+    if (this.moving) {
+      if (event) {
+        event.preventDefault();
+      }
+      this.stopMoveLeft.emit();
+      this.stopMoveUp.emit();
+      this.stopMoveRight.emit();
+      this.stopMoveDown.emit();
+    }
   };
 }
